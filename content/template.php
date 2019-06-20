73a8a1b8aea0b926dd17a6919ccea0fe6a5a60c9
Vladislav Rodionov <vladislav.for@mail.ru>
2019-06-20 14:47:23 +1200

    create

---
+++ content/template.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+
+<head>
+    <title>Home</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css"
+        href="//fonts.googleapis.com/css?family=Lato:300,400,700,400italic%7CJosefin+Sans:400,700,300italic">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/artplayer.css">
+    <!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+</head>
+
+<body>
+
+    <div id="page-loader">
+        <div class="page-loader-body">
+            <div class="cssload-spinner">
+                <div class="cssload-cube cssload-cube0"></div>
+                <div class="cssload-cube cssload-cube1"></div>
+                <div class="cssload-cube cssload-cube2"></div>
+                <div class="cssload-cube cssload-cube3"></div>
+                <div class="cssload-cube cssload-cube4"></div>
+                <div class="cssload-cube cssload-cube5"></div>
+                <div class="cssload-cube cssload-cube6"></div>
+                <div class="cssload-cube cssload-cube7"></div>
+                <div class="cssload-cube cssload-cube8"></div>
+                <div class="cssload-cube cssload-cube9"></div>
+                <div class="cssload-cube cssload-cube10"></div>
+                <div class="cssload-cube cssload-cube11"></div>
+                <div class="cssload-cube cssload-cube12"></div>
+                <div class="cssload-cube cssload-cube13"></div>
+                <div class="cssload-cube cssload-cube14"></div>
+                <div class="cssload-cube cssload-cube15"></div>
+            </div>
+        </div>
+    </div>
+    <!-- Page-->
+
+    <? include "part/header.php"; ?>
+    <main>
+    <? include $viewway; ?>
+    </main>
+    <? include "part/footer.php"; ?>
+
+
+
+    
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+   
+    <script src="js/artplayer.js"></script>
+    <script src="js/script.js"></script>
+    
+    <!-- coded by starlight-->
+</body>
+
+</html>
+</body>
+
+</html>
---
+++ content/views/home.php
@@ -0,0 +1,243 @@
+﻿
+ <div class="artplayer-app"></div>
+
+<div class="page">
+        <!-- Page header-->
+        <header class="section page-header">
+            <!-- RD Navbar-->
+            <div class="rd-navbar-wrap">
+                <nav class="rd-navbar" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed"
+                    data-sm-device-layout="rd-navbar-fixed" data-md-layout="rd-navbar-static"
+                    data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-static"
+                    data-lg-layout="rd-navbar-static" data-stick-up-clone="false" data-md-stick-up-offset="5px"
+                    data-lg-stick-up-offset="5px" data-md-stick-up="true" data-lg-stick-up="true">
+                    <div class="rd-navbar-main-outer">
+                        <div class="rd-navbar-main">
+                            <!-- RD Navbar Panel-->
+                            <div class="rd-navbar-panel">
+                                <button class="rd-navbar-toggle"
+                                    data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                                <!-- RD Navbar Brand-->
+                                <div class="rd-navbar-brand"><a class="brand" href="index.html">
+                                        <div class="brand__name"><img class="brand__logo-dark"
+                                                src="images/logo-243x57.png" alt="" width="243" height="57" /><img
+                                                class="brand__logo-light" src="images/logo-inverse-243x57.png" alt=""
+                                                width="243" height="57" />
+                                        </div>
+                                    </a></div>
+                            </div>
+                            <!-- RD Navbar Nav-->
+                            <div class="rd-navbar-nav-wrap">
+                                <div class="rd-navbar-element">
+                                    <ul class="list-icons list-inline-sm">
+
+                                        <li><a class="icon icon-sm fa fa-telegram icon-style-camera"
+                                                href="#"><span></span><span></span><span></span><span></span></a></li>
+                                    </ul>
+                                </div>
+                                <!-- RD Navbar Nav-->
+                                <ul class="rd-navbar-nav">
+                                    <li class="active"><a href="index.html">Home</a>
+                                    </li>
+                                    <li><a href="about-me.html">Videos</a>
+                                    </li>
+                                    <li><a href="typography.html">Photos</a>
+                                    </li>
+                                    <li><a href="contact-me.html">Comics</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <section class="section bg-white text-center">
+            <!-- Slick Carousel-->
+            <div class="slick-wrap">
+                <div class="slick-slider slick-style-1" data-arrows="true" data-autoplay="false" data-loop="true"
+                    data-dots="true" data-swipe="true" data-xs-swipe="true" data-sm-swipe="false" data-items="1"
+                    data-sm-items="3" data-md-items="3" data-lg-items="3" data-center-mode="true"
+                    data-lightgallery="group-slick">
+
+
+
+
+                    <div class="item slick-slide slick-current slick-active slick-center" data-slick-index="0"
+                        aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide00">
+                        <div class="slick-slide-inner">
+                            <div class="slick-slide-caption"><a class="thumb-ann thumb-mixed_large"
+                                    href="images/kdAdetL_QBk.jpg" data-lightgallery="item" tabindex="0"><img
+                                        class="thumb-ann__image"
+                                        src="images/kdAdetL_QBk.jpg" alt=""
+                                        width="961" height="664">
+                                    <div class="thumb-ann__caption">
+                                        <p class="thumb-ann__title heading-3">Lesbian </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item slick-slide slick-current slick-active slick-center" data-slick-index="0"
+                        aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide00">
+                        <div class="slick-slide-inner">
+                            <div class="slick-slide-caption"><a class="thumb-ann thumb-mixed_large"
+                                    href="images/NlT7V71.jpg" data-lightgallery="item"
+                                    tabindex="0"><img class="thumb-ann__image"
+                                        src="images/NlT7V71.jpg" alt=""
+                                        width="961" height="664">
+                                    <div class="thumb-ann__caption">
+                                        <p class="thumb-ann__title heading-3">Lesbian </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item slick-slide slick-current slick-active slick-center" data-slick-index="0"
+                        aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide00">
+                        <div class="slick-slide-inner">
+                            <div class="slick-slide-caption"><a class="thumb-ann thumb-mixed_large"
+                                    href="images/tTxaS-KisLQ.jpg data-lightgallery="item"
+                                    tabindex="0"><img class="thumb-ann__image"
+                                        src="images/tTxaS-KisLQ.jpg" alt=""
+                                        width="961" height="664">
+                                    <div class="thumb-ann__caption">
+                                        <p class="thumb-ann__title heading-3">Lesbian </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item slick-slide slick-current slick-active slick-center" data-slick-index="0"
+                        aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide00">
+                        <div class="slick-slide-inner">
+                            <div class="slick-slide-caption"><a class="thumb-ann thumb-mixed_large"
+                                    href="images/group-image-4-1200x800-original.jpg" data-lightgallery="item"
+                                    tabindex="0"><img class="thumb-ann__image"
+                                        src="https://pp.userapi.com/c543101/v543101951/5418e/kdAdetL_QBk.jpg" alt=""
+                                        width="961" height="664">
+                                    <div class="thumb-ann__caption">
+                                        <p class="thumb-ann__title heading-3">Lesbian </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </section>
+
+        <section class="section section-md bg-white text-center">
+            <div class="shell-fluid">
+                <p class="heading-1">Video</p>
+                <div class="isotope thumb-ruby-wrap wow fadeIn" data-isotope-layout="masonry"
+                    data-isotope-group="gallery" data-lightgallery="group">
+                    <div class="row">
+                   
+                    </div>
+                </div>
+            </div>
+        </section>
+
+      
+
+        <!-- Book Your Photo Shoot-->
+        <section class="section section-md bg-white">
+            <div class="shell">
+                <div class="range range-50 range-sm-center range-md-left range-md-reverse range-md-middle">
+                    <div class="cell-sm-10 cell-md-6 wow fadeInRight">
+                        <div class="box-width-4 box-centered">
+                            <p class="heading-1">Book Your <br> Photo Shoot</p>
+                            <p>I will appreciate as many details about your particular wedding venue, dates, theme and
+                                other aspects,
+                                as you’d be willing to share. I will be giving you my feedback immediately!</p>
+                        </div>
+                    </div>
+                    <div class="cell-sm-10 cell-md-6 wow fadeInLeft">
+                        <article class="box-line"><span></span><span></span><span></span><span></span>
+                            <div class="box-line__main">
+                                <!-- RD Mailform-->
+                                <form class="rd-mailform" data-form-output="form-output-global" data-form-type="contact"
+                                    method="post" action="bat/rd-mailform.php">
+                                    <div class="form-wrap">
+                                        <input class="form-input" id="contact-date" type="text" data-time-picker="date"
+                                            name="date" data-constraints="@Required">
+                                        <label class="form-label" for="contact-date">Event Date</label>
+                                    </div>
+                                    <div class="form-wrap">
+                                        <input class="form-input" id="contact-name" type="text" name="name"
+                                            data-constraints="@Required">
+                                        <label class="form-label" for="contact-name">Your Name</label>
+                                    </div>
+                                    <div class="form-wrap">
+                                        <input class="form-input" id="contact-email" type="email" name="email"
+                                            data-constraints="@Email @Required">
+                                        <label class="form-label" for="contact-email">E-mail</label>
+                                    </div>
+                                    <div class="form-wrap">
+                                        <label class="form-label" for="contact-message">Your Message</label>
+                                        <textarea class="form-input" id="contact-message" name="message"
+                                            data-constraints="@Required"></textarea>
+                                    </div>
+                                    <div class="form-wrap form-button offset-1">
+                                        <button class="button button-block button-primary-outline button-ujarak"
+                                            type="submit">make an
+                                            appointment</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Page Footer--><a class="section section-banner"
+            href="https://www.templatemonster.com/intense-multipurpose-html-template.html" target="_blank"
+            style="background-image: url(images/background-01-1920x310.jpg); background-image: -webkit-image-set( url(images/background-01-1920x310.jpg) 1x, url(images/background-01-3840x620.jpg) 2x )"><img
+                src="images/foreground-01-1600x310.png"
+                srcset="images/foreground-01-1600x310.png 1x, images/foreground-01-3200x620.png 2x" alt="" width="1600"
+                height="310"></a>
+        <footer class="footer-centered section bg-gray-darker">
+            <div class="shell">
+                <div class="range range-sm-center">
+                    <div class="cell-sm-10 cell-md-8 cell-lg-6">
+                        <!-- Brand--><a class="brand" href="index.html">
+                            <div class="brand__name"><img src="images/logo-inverse-243x57.png" alt="" width="243"
+                                    height="57" />
+                            </div>
+                        </a>
+                        <!-- RD Mailform-->
+                        <form class="rd-mailform form_inline" data-form-output="form-output-global"
+                            data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
+                            <div class="form__inner">
+                                <div class="form-wrap">
+                                    <input class="form-input" id="subscribe-form-footer-email" type="email" name="email"
+                                        data-constraints="@Email @Required">
+                                    <label class="form-label" for="subscribe-form-footer-email">Your E-mail...</label>
+                                </div>
+                                <div class="form-button">
+                                    <button class="button button-link" type="submit">Subscribe</button>
+                                </div>
+                            </div>
+                        </form>
+                        <ul class="list-icons list-inline-sm">
+                            <li><a class="icon icon-sm fa fa-instagram icon-style-camera"
+                                    href="#"><span></span><span></span><span></span><span></span></a></li>
+                            <li><a class="icon icon-sm fa fa-facebook icon-style-camera"
+                                    href="#"><span></span><span></span><span></span><span></span></a></li>
+                            <li><a class="icon icon-sm fa fa-pinterest icon-style-camera"
+                                    href="#"><span></span><span></span><span></span><span></span></a></li>
+                        </ul>
+                        <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span>. All Rights
+                            Reserved. Design
+                            by <a href="https://www.templatemonster.com">TemplateMonster</a></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
